5e2e3ef57fd051587724885182e8220ce96e849a
Vishal Patel <vishalpatel2890@gmail.com>
2017-08-20 15:05:10 -0400

    added Dockerfile

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React on Docker!</h2>
         </div>
         <p className="App-intro">
-          <RaisedButton label="Default" />
+          <RaisedButton label="New" />
         </p>
       </div>
     );
